78ba9c9ee7d7e926cfb4c1db403d9e8a34473edb
Gustavo Bender <gustavo.benderr@gmail.com>
2024-09-17 17:12:57 -0300

    feat: Adiciona diferentes tipos de builds

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
-cmake CMakeLists.txt
-cmake --build .
+#!/bin/bash
+# Versão para release
+#cmake -DCMAKE_BUILD_TYPE=Release CMakeLists.txt 
+
+# Versão para debug
+cmake -DCMAKE_BUILD_TYPE=Debug CMakeLists.txt 
+
+# Build o projeto
+make
+
+# Executa o programa
 ./target/ProjetoCG1
+
+# Limpa arquivos gerados pelo cmake e make
+# rm -rf cmake_install.cmake CMakeFiles/ CMakeCache.txt Makefile target
