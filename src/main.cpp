7e7ed9f3d2a483a3527467c98e94116eb41a6bfb
Gustavo Bender <gustavo.benderr@gmail.com>
2024-09-17 20:52:26 -0300

    feat: Adiciona modificações para suportar as novas classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
+#include "Flowers/Flower.hpp"
+#include "Flowers/Seeder.hpp"
 #include <GL/glut.h>
 #include <cmath>
 #include <iostream>
+#include <vector>
 
-GLfloat angle, fAspect, largura, altura, ycamera;
+GLfloat angle, fAspect, largura, altura;
+GLfloat xcamera = 0, ycamera = 0, zcamera = 300;
+GLfloat xtarget = 0, ytarget = 0, ztarget = 0;
+GLfloat look_angle = 0;
+GLfloat xvector = 0, yvector = 1.0, zvector = 0;
+std::vector<Flower *> flowers;
 
 void Desenha(void) {
   glClear(GL_COLOR_BUFFER_BIT);
@@ -15,13 +23,16 @@ void Desenha(void) {
   // glutWireCube(50.0);
   // glutWireSphere(50, 20, 20);
 
-  glColor3f(1.0f, 0.2f, 0.2f);
-  glPushMatrix();
-  glTranslated(0, 0, 0);
-  glutWireTeapot(50.0f);
-  // glColor3f(0.0f, 0.2f, 0.2f); glutWireSphere(50, 20, 20); glColor3f(1.0f,
-  // 0.2f, 0.0f); glutWireCube(50.0);
-  glPopMatrix();
+  // glColor3f(1.0f, 0.2f, 0.2f);
+  // glPushMatrix();
+  // glTranslated(0, 0, 0);
+  // glutWireTeapot(25.0f);
+  // // glColor3f(0.0f, 0.2f, 0.2f); glutWireSphere(50, 20, 20); glColor3f(1.0f,
+  // // 0.2f, 0.0f); glutWireCube(50.0);
+  // glPopMatrix();
+  for (Flower *i : flowers) {
+    i->draw_flower();
+  }
   /*
           // Desenha um cubo utilizando primitivas
           glColor3f(0.7f, 0.4f, 1.0f);
@@ -35,15 +46,10 @@ void Desenha(void) {
                   glColor3f(0.0f, 0.0f, 1.0f);
           glBegin(GL_QUADS);			// Face posterior
                   glNormal3f(0.0, 0.0, -1.0);	// Normal da face
-                  glVertex3f(40.0, 40.0, 0.0);
-                  glVertex3f(40.0, -40.0, 0.0);
-                  glVertex3f(-40.0, -40.0, 0.0);
-                  glVertex3f(-40.0, 40.0, 0.0);
-          glEnd();
-          glColor3f(1.0f, 1.0f, 0.0f);
-          glBegin(GL_QUADS);			// Face inferior
-                  glNormal3f(0.0, -1.0, 0.0); 	// Normal da face
-                  glVertex3f(-40.0, -40.0, 0.0);
+  glVertex3f(40.0, 40.0, 0.0); glVertex3f(40.0, -40.0, 0.0); glVertex3f(-40.0,
+  -40.0, 0.0); glVertex3f(-40.0, 40.0, 0.0); glEnd(); glColor3f(1.0f, 1.0f,
+  0.0f); glBegin(GL_QUADS);			// Face inferior glNormal3f(0.0,
+  -1.0, 0.0); 	// Normal da face glVertex3f(-40.0, -40.0, 0.0);
                   glVertex3f(40.0, -40.0, 0.0);
                   glVertex3f(40.0, -40.0, 80.0);
                   glVertex3f(-40.0, -40.0, 80.0);
@@ -86,8 +92,14 @@ void Inicializa(void) {
   angle = 45;
 }
 
+void set_look_angle() {
+  xvector = std::sin(look_angle);
+  yvector = std::cos(look_angle);
+}
+
 // Função usada para especificar o volume de visualização
 void EspecificaParametrosVisualizacao(void) {
+  Seeder *sed = Seeder::GetInstance();
   // Especifica sistema de coordenadas de projeção
   glMatrixMode(GL_PROJECTION);
   // Inicializa sistema de coordenadas de projeção
@@ -100,10 +112,12 @@ void EspecificaParametrosVisualizacao(void) {
   // Inicializa sistema de coordenadas do modelo
   glLoadIdentity();
 
+  set_look_angle();
+
   // Especifica posição do observador e do alvo
-  gluLookAt(0, ycamera, 300, // posição da câmera
-            0, 0, 0,         // posição do alvo
-            0, 1, 0);        // vetor UP da câmera
+  gluLookAt(sed->get_x(), sed->get_y(), sed->get_z(), // posição da câmera
+            xtarget, ytarget, ztarget,                // posição do alvo
+            xvector, yvector, zvector);               // vetor UP da câmera
 }
 
 // Função callback chamada quando o tamanho da janela é alterado
@@ -137,42 +151,55 @@ void GerenciaMouse(int button, int state, int x, int y) {
   EspecificaParametrosVisualizacao();
   glutPostRedisplay();
 }
-/*
-void TeclasEspeciais(int key, int x, int y)
-{
-        if (key == GLUT_KEY_UP) {
-                ........;
-        }
-        if (key == GLUT_KEY_DOWN) {
-                ........;
-        }
-        if (key == GLUT_KEY_RIGHT) {
-                ........;
-        }
-        if (key == GLUT_KEY_LEFT) {
-                ........;
-        }
-        EspecificaParametrosVisualizacao();
-        glutPostRedisplay();
+
+void TeclasEspeciais(int key, int x, int y) {
+  Seeder *sed = Seeder::GetInstance();
+  if (key == GLUT_KEY_UP) {
+    sed->move_z(-10);
+    ztarget -= 10;
+  }
+  if (key == GLUT_KEY_DOWN) {
+    sed->move_z(10);
+    ztarget += 10;
+  }
+  if (key == GLUT_KEY_RIGHT) {
+    sed->move_x(10);
+    xtarget += 10;
+  }
+  if (key == GLUT_KEY_LEFT) {
+    sed->move_x(-10);
+    xtarget -= 10;
+  }
+  EspecificaParametrosVisualizacao();
+  glutPostRedisplay();
 }
 
 void GerenciaTeclado(unsigned char key, int x, int y) {
-        switch (key) {
-                case ' ': // restaura posição inicial da camera
-                        ............
-                        break;
-                // movimentacao do observador
-                case 'd':
-                        ..............
-                        break;
-                case 'a':
-                        ...............
-                        break;
-        }
-        EspecificaParametrosVisualizacao();
-        glutPostRedisplay();
+  Seeder *sed = Seeder::GetInstance();
+  switch (key) {
+  case ' ': // restaura posição inicial da camera
+    // xcamera = 0, ycamera = 0, zcamera = 300;
+    // xtarget = 0, ytarget = 0, ztarget = 0;
+    // look_angle = 0;
+    // xvector = 0, yvector = 1.0, zvector = 0;
+    flowers.push_back(sed->generate_flower());
+    break;
+  // movimentacao do observador
+  case 'd':
+    break;
+  case 'a':
+    break;
+  case 'q':
+    look_angle += 0.3;
+    break;
+  case 'e':
+    look_angle -= 0.3;
+    break;
+  }
+  EspecificaParametrosVisualizacao();
+  glutPostRedisplay();
 }
-*/
+
 int main(int argc, char **argv) {
   glutInit(&argc, argv);
 
@@ -180,19 +207,19 @@ int main(int argc, char **argv) {
                       GLUT_RGB); // GLUT_DOUBLE trabalha com dois buffers: um
                                  // para renderização e outro para exibição
 
-  glutInitWindowPosition(700, 100);
-  largura = 600;
-  altura = 500;
+  glutInitWindowPosition(300, 50);
+  largura = 1200;
+  altura = 600;
   glutInitWindowSize(largura, altura);
   fAspect = (GLfloat)largura / (GLfloat)altura;
-  ycamera = 0;
   glutCreateWindow("Aula Pratica 4");
 
   glutDisplayFunc(Desenha);
   glutReshapeFunc(AlteraTamanhoJanela); // Função para ajustar o tamanho da tela
   glutMouseFunc(GerenciaMouse);
-  // glutKeyboardFunc(GerenciaTeclado); // Define qual funcao gerencia o
-  // comportamento do teclado glutSpecialFunc(TeclasEspeciais); // Define qual
+  glutKeyboardFunc(GerenciaTeclado); // Define qual funcao gerencia o
+  // comportamento do teclado
+  glutSpecialFunc(TeclasEspeciais); // Define qual
   // funcao gerencia as teclas especiais
   Inicializa();
   glutMainLoop();
